dc9940fe4ae39cb9dd87a9e17831304723b6337d
Alex Feng <35203563+gearsmotion789@users.noreply.github.com>
2019-04-25 11:21:57 -0700

    Update melodic.sh

--- melodic.sh
+++ melodic.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt update
-sudo apt install ros-melodic-desktop-full
+sudo apt install ros-melodic-desktop-full -y
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
@@ -13,8 +13,8 @@ sudo apt install python-rosinstall python-rosinstall-generator python-wstool bui
 rosdep update
 sudo apt-get update
 sudo apt-get dist-upgrade
-sudo apt-get install ros-melodic-catkin python-catkin-tools
-sudo apt-get install ros-melodic-moveit
+sudo apt-get install ros-melodic-catkin python-catkin-tools -y
+sudo apt-get install ros-melodic-moveit -y
 mkdir -p ~/a-ws/src
 cd ~/a-ws/src
 git clone https://github.com/ros-planning/moveit_tutorials.git -b melodic-devel
@@ -29,10 +29,10 @@ source ~/.bashrc
 # Install Atom Editor
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-sudo apt-get install atom
+sudo apt-get install atom -y
 
 # Install Gazebo
-sudo apt-get install curl
+sudo apt-get install curl -y
 curl -sSL http://get.gazebosim.org | sh
 
 echo "INSTALLATION DONE!"
