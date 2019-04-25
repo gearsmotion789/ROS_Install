77567e8fb1c2e95ce51aa4a66404306507174f04
Alex Feng <35203563+gearsmotion789@users.noreply.github.com>
2019-04-25 10:52:44 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# Install ROS Kinetic
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
@@ -8,6 +9,10 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
 
+# Install Gazebo
+curl -sSL http://get.gazebosim.org | sh
+
+# Install MoveIt & samples
 sudo apt-get install ros-kinetic-catkin python-catkin-tools
 sudo apt-get install ros-kinetic-moveit
 mkdir -p ~/a-ws/src
