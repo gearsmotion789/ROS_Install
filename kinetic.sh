27694e8a5170ff3335a86682c169b1e8629ffe54
Alex Feng <35203563+gearsmotion789@users.noreply.github.com>
2019-05-08 14:17:56 -0700

    Update kinetic.sh

--- kinetic.sh
+++ kinetic.sh
@@ -1,13 +1,5 @@
-# Install ROS Kinetic
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
-sudo apt-get -y install ros-kinetic-desktop-full
-sudo rosdep init
-rosdep update
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+# Install ROS Kinetic & Gazebo
+git clone https://github.com/linorobot/rosme && cd rosme && ./install
 
 # Install MoveIt & samples
 sudo apt-get -y install ros-kinetic-catkin python-catkin-tools
@@ -29,9 +21,5 @@ sourc ~/.bashrc
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get -y install atom
-
-# Install Gazebo
-sudo apt-get -y install curl
-curl -sSL http://get.gazebosim.org | sh
 
 echo "INSTALLATION DONE!"
