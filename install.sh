22e902cdc0adee647d49a58cc3fd010e75ebfd37
Alex Feng <35203563+gearsmotion789@users.noreply.github.com>
2019-05-08 13:47:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get -y install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
@@ -10,8 +10,8 @@ source ~/.bashrc
 sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
 
 # Install MoveIt & samples
-sudo apt-get install ros-kinetic-catkin python-catkin-tools
-sudo apt-get install ros-kinetic-moveit
+sudo apt-get -y install ros-kinetic-catkin python-catkin-tools
+sudo apt-get -y install ros-kinetic-moveit
 mkdir -p ~/a-ws/src
 cd ~/a-ws/src
 sudo apt-get install git
@@ -25,10 +25,10 @@ sourc ~/.bashrc
 # Install Atom Editor
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-sudo apt-get install atom
+sudo apt-get -y install atom
 
 # Install Gazebo
-sudo apt-get install curl
+sudo apt-get -y install curl
 curl -sSL http://get.gazebosim.org | sh
 
 echo "INSTALLATION DONE!"
